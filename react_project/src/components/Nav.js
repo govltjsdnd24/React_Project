1a9ed1ecd6b88480828cee95d592d08d1ef8bb87
gragasolaf <gragaolaf@gmail.com>
2024-02-20 16:58:47 +0900

    Initialization

---
+++ react_project/src/components/Nav.js
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from "react";
+import { NavLink,useNavigate} from "react-router-dom";
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
+import "../styles/common/Nav.scss";
+
+function Nav() {
+  const navigate = useNavigate();
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [size, setSize] = useState({
+    width: 0,
+    height: 0,
+  });
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (size.width > 768 && menuOpen) {
+      setMenuOpen(false);
+    }
+  }, [size.width, menuOpen]);
+
+  const menuToggleHandler = () => {
+    setMenuOpen((p) => !p);
+  };
+
+  return (
+    <header className="header">
+      <div className="header__content">
+        <NavLink to="/" className="header__content__logo">
+          React Project
+        </NavLink>
+        <nav
+          className={`${"header__content__nav"} 
+          ${menuOpen && size.width < 768 ? `${"isMenu"}` : ""} 
+          }`}
+        >
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/mypage">My page</NavLink>
+            </li>
+            <li>
+              <NavLink to="/forum">Forum</NavLink>
+            </li>
+            <li>
+              <NavLink to="/map">Map</NavLink>
+            </li>
+
+            {/* <NavLink to="/register">
+              <button className="btn">Register</button>
+            </NavLink>
+            <NavLink to="/login">
+              <button className="btn btn__login">Login</button>
+            </NavLink> */}
+          </ul>
+        </nav>
+        <div className="header__content__toggle">
+          {!menuOpen ? (
+            <BiMenuAltRight onClick={menuToggleHandler} />
+          ) : (
+            <AiOutlineClose onClick={menuToggleHandler} />
+          )}
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Nav;
