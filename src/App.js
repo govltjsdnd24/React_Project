6ad9d40fbb1980268fb2379d16438f83e1422c04
Sun Woong Chang <38126462+govltjsdnd24@users.noreply.github.com>
2024-03-06 16:23:00 +0900

    animation-model

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import { Route, Routes } from "react-router-dom";
 import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import MyPage from "./pages/MyPage";
 import Forum from "./pages/Forum";
 import Map from "./pages/Map";
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
-import {Model} from './Scene'
-
+import { Canvas,useFrame } from '@react-three/fiber';
+import { OrbitControls, KeyboardControls,Gltf,Environment} from '@react-three/drei';
+import { useSpring, animated, config } from "@react-spring/three";
+import { Physics, RigidBody, MeshCollider } from '@react-three/rapier'
+import Model from './Hiphop'
 import "./styles/common/App.css";
+import Controller from 'ecctrl'
+
+function OscillatingMan() {
+  const myMesh = useRef();
+  const { scale } = useSpring({
+    scale: [1, 1, 1], 
+    config: config.default
+  });
+
+  useFrame(({ clock }) => {
+    const time_position = 5 * clock.getElapsedTime();
+    const oscillation_position = Math.sin(time_position);
+
+    // if (myMesh.current) {
+    //   myMesh.current.position.y =  oscillation_position;
+    // }
+  });
+
+  return (
+    <animated.mesh
+      scale={scale}
+      ref={myMesh}
+      position={[0, -1, 0]}
+    >
+      <Model />
+    </animated.mesh>
+  );
+}
 
 function App() {
+  const keyboardMap = [
+    { name: 'forward', keys: ['ArrowUp', 'KeyW'] },
+    { name: 'backward', keys: ['ArrowDown', 'KeyS'] },
+    { name: 'leftward', keys: ['ArrowLeft', 'KeyA'] },
+    { name: 'rightward', keys: ['ArrowRight', 'KeyD'] },
+    { name: 'jump', keys: ['Space'] },
+    { name: 'run', keys: ['Shift'] },
+  ]
   return (
     <div className="App">
       <Nav />
@@ -22,13 +59,26 @@ function App() {
         <Route path="/map" element={<Map />} />
       </Routes>
       <Canvas>
-      <OrbitControls/>
-        <mesh>
-          <ambientLight intensity={1}/>
-          <directionalLight intensity={1}/>
-          <Model/>
-          <meshStandardMaterial attach="material" color= {0xa3b18a}/>
-        </mesh>
+        <OrbitControls />
+        
+        <ambientLight intensity={1} />
+        <directionalLight intensity={1} />
+        <Physics timeStep="vary">
+          <KeyboardControls map={keyboardMap}>
+            <Controller maxVelLimit={5}>
+              <RigidBody colliders={false} gravityScale={4}>
+                <OscillatingMan /> 
+              </RigidBody>
+            </Controller>
+          </KeyboardControls>
+
+          <RigidBody type="fixed">
+            <mesh>
+              <boxGeometry attach="geometry" args={[10, 1, 10]} />
+              <meshBasicMaterial attach="material" color="gray" />
+            </mesh>
+          </RigidBody>
+        </Physics>
       </Canvas>
     </div>
   );
